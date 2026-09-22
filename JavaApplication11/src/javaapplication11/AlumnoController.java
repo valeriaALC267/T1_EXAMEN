29ede8ce5e54a66be19be036f82ceb9c05fd0660
Valeria Hinostroza <valeriaalcalde65@gmail.com>
2026-09-21 19:27:30 -0500

    agregar validaciones en setters

--- JavaApplication11/src/javaapplication11/AlumnoController.java
+++ JavaApplication11/src/javaapplication11/AlumnoController.java
@@ -29,7 +29,12 @@ public class AlumnoController
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void setNombre(String nombre) 
+    {
+        if(nombre == null || nombre.isEmpty())
+        {
+            throw new IllegalArgumentException("Ups! Debe llenar este campo primero, por favor.");
+        }
         this.nombre = nombre;
     }
 
@@ -37,7 +42,12 @@ public class AlumnoController
         return apellido_p;
     }
 
-    public void setApellido_p(String apellido_p) {
+    public void setApellido_p(String apellido_p)
+    {
+         if(apellido_p== null || apellido_p.isEmpty())
+        {
+            throw new IllegalArgumentException("Ups! Debe llenar este campo primero, por favor.");
+        }
         this.apellido_p = apellido_p;
     }
 
@@ -45,7 +55,12 @@ public class AlumnoController
         return apellido_m;
     }
 
-    public void setApellido_m(String apellido_m) {
+    public void setApellido_m(String apellido_m)
+    {
+         if(apellido_m == null || apellido_m.isEmpty())
+        {
+            throw new IllegalArgumentException("Ups! Debe llenar este campo primero, por favor.");
+        }
         this.apellido_m = apellido_m;
     }
 
@@ -53,7 +68,16 @@ public class AlumnoController
         return tipo_doc;
     }
 
-    public void setTipo_doc(String tipo_doc) {
+    public void setTipo_doc(String tipo_doc) 
+    {
+         if(tipo_doc == null || tipo_doc.isEmpty())
+        {
+            throw new IllegalArgumentException("Ups! Debe llenar este campo primero, por favor.");
+        }
+         if(!tipo_doc.equalsIgnoreCase("dni") && !tipo_doc.equalsIgnoreCase("cr"))
+         {
+             throw new IllegalArgumentException("Ups! Este tipo de documento no es valido. Intente de nuevo, por favor");
+         }
         this.tipo_doc = tipo_doc;
     }
 
@@ -61,15 +85,39 @@ public class AlumnoController
         return nro_doc;
     }
 
-    public void setNro_doc(String nro_doc) {
+    public void setNro_doc(String nro_doc) 
+    {
+        if(nro_doc == null || nro_doc.isEmpty())
+        {
+            throw new IllegalArgumentException("Ups! Debe llenar este campo primero, por favor.");
+        }
+        if(nro_doc.length() != 8 && this.tipo_doc.equalsIgnoreCase("dni"))
+        {
+            throw new IllegalArgumentException("Ups! El DNI debe tener 8 digitos. Intente de nuevo, por favor.");
+        }
+        else if(nro_doc.length() != 11 && this.tipo_doc.equalsIgnoreCase("cr"))
+        {
+            throw new IllegalArgumentException("Ups! El Carnet de residencia temporal debe tener 11 digitos. Intente de nuevo, por favor.");
+        }
         this.nro_doc = nro_doc;
     }
 
-    public String getNv_socioec() {
+    public String getNv_socioec() 
+    {
+        
         return nv_socioec;
     }
 
-    public void setNv_socioec(String nv_socioec) {
+    public void setNv_socioec(String nv_socioec)
+    {
+        if(nv_socioec == null || nv_socioec.isEmpty())
+        {
+            throw new IllegalArgumentException("Ups! Debe llenar este campo primero, por favor.");
+        }
+        if(!nv_socioec.equalsIgnoreCase("A") && !nv_socioec.equalsIgnoreCase("B")&& !nv_socioec.equalsIgnoreCase("C"))
+        {
+            throw new IllegalArgumentException("Ups! Este tipo se nivel socioeconomico no es valido. Intente de nuevo, por favor.");
+        }
         this.nv_socioec = nv_socioec;
     }
 
@@ -77,7 +125,16 @@ public class AlumnoController
         return tipo_beca;
     }
 
-    public void setTipo_beca(String tipo_beca) {
+    public void setTipo_beca(String tipo_beca)
+    {
+        if(tipo_beca == null || tipo_beca.isEmpty())
+        {
+            throw new IllegalArgumentException("Ups! Debe llenar este campo primero, por favor.");
+        }
+        if(!tipo_beca.equalsIgnoreCase("total") && !tipo_beca.equalsIgnoreCase("parcial"))
+        {
+            throw new IllegalArgumentException("Ups! Este tipo de beca no es valido. Intente de nuevo, por favor");
+        }
         this.tipo_beca = tipo_beca;
     }
     
