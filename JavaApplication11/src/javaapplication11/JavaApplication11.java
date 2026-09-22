968428160e4d292efdb26f514972a350549b9e99
Valeria Hinostroza <valeriaalcalde65@gmail.com>
2026-09-21 20:01:25 -0500

    agregar ingreso de datos en main

--- JavaApplication11/src/javaapplication11/JavaApplication11.java
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -1,20 +1,127 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package javaapplication11;
 
-/**
- *
- * @author UCA40409
- */
+import java.util.Scanner;
+
+
 public class JavaApplication11 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args)
+    {
+        Scanner teclado = new Scanner(System.in);
+        AlumnoController ac = new AlumnoController();
+        String r;
+        
+        do 
+        {            
+            System.out.println("------------------REGISTRO DE ALUMNOS ISTITUTO INNOVA-----------------------");
+            Alumno a = new Alumno();
+            Boolean n = false;
+            while(!n)
+            {
+                try 
+                {
+                    System.out.println("Nombre:");
+                    a.setNombre(teclado.nextLine());
+                    n = true;
+                } 
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            Boolean ap = false;
+            while(!ap)
+            {
+                try 
+                {
+                    System.out.println("Apellido Paterno:");
+                    a.setApellido_p(teclado.nextLine());
+                    ap = true;
+                } 
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            Boolean am = false;
+            while(!am)
+            {
+                try 
+                {
+                    System.out.println("Apellido Materno:");
+                    a.setApellido_m(teclado.nextLine());
+                    am = true;
+                } 
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            Boolean td = false;
+            while(!td)
+            {
+                try 
+                {
+                    System.out.println("Tipo Documento:");
+                    a.setTipo_doc(teclado.nextLine());
+                    td = true;
+                } 
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            Boolean nd = false;
+            while(!nd)
+            {
+                try 
+                {
+                    System.out.println("Numero Documento:");
+                    a.setNro_doc(teclado.nextLine());
+                    nd = true;
+                } 
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            Boolean nv = false;
+            while(!nv)
+            {
+                try 
+                {
+                    System.out.println("Nivel Socioeconomico:");
+                    a.setNv_socioec(teclado.nextLine());
+                    nv = true;
+                } 
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            Boolean b = false;
+            while(!b)
+            {
+                try 
+                {
+                    System.out.println("Tipo beca:");
+                    a.setNombre(teclado.nextLine());
+                    b = true;
+                } 
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } while (true);
     }
     
 }
