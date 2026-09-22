9977489bdba75b6f3538a3db41db47514f253a3f
Valeria Hinostroza <valeriaalcalde65@gmail.com>
2026-09-21 19:50:50 -0500

    agregar metodos 'agregar' y 'listar' en alumnoController

--- JavaApplication11/src/javaapplication11/AlumnoController.java
+++ JavaApplication11/src/javaapplication11/AlumnoController.java
@@ -11,4 +11,34 @@ public class AlumnoController
     {
         listaAlumnos = new ArrayList<>();
     }
+    public Boolean VerDuplicados(String doc)
+    {
+        for(int i = 0; i<listaAlumnos.size(); i++)
+        {
+            if(doc.equalsIgnoreCase(listaAlumnos.get(i).getNro_doc()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void Agregar(Alumno a)
+    {
+        if(VerDuplicados(a.getNro_doc()))
+        {
+            System.out.println("Ups! No se puede registrar a este alumno. Ya existe uno con el mismo numero de documento");
+        }
+        else
+        {
+          listaAlumnos.add(a);
+            System.out.println("Alumno registrado exitosamente!");
+        }
+    }
+    public void Listar()
+    {
+        for(Alumno a : listaAlumnos)
+        {
+            System.out.println(a.MostrarDatos());
+        }
+    }
 }
