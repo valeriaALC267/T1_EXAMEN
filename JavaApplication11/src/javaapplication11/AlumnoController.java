bd72e6a6ef1554dbb7ba0f2d8e8c1e115196935b
Valeria Hinostroza <valeriaalcalde65@gmail.com>
2026-09-21 19:16:52 -0500

    creacion de getters y setters en clase 'alumno'

--- JavaApplication11/src/javaapplication11/AlumnoController.java
+++ JavaApplication11/src/javaapplication11/AlumnoController.java
@@ -24,5 +24,61 @@ public class AlumnoController
         this.nv_socioec = nv_socioec;
         this.tipo_beca = tipo_beca;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido_p() {
+        return apellido_p;
+    }
+
+    public void setApellido_p(String apellido_p) {
+        this.apellido_p = apellido_p;
+    }
+
+    public String getApellido_m() {
+        return apellido_m;
+    }
+
+    public void setApellido_m(String apellido_m) {
+        this.apellido_m = apellido_m;
+    }
+
+    public String getTipo_doc() {
+        return tipo_doc;
+    }
+
+    public void setTipo_doc(String tipo_doc) {
+        this.tipo_doc = tipo_doc;
+    }
+
+    public String getNro_doc() {
+        return nro_doc;
+    }
+
+    public void setNro_doc(String nro_doc) {
+        this.nro_doc = nro_doc;
+    }
+
+    public String getNv_socioec() {
+        return nv_socioec;
+    }
+
+    public void setNv_socioec(String nv_socioec) {
+        this.nv_socioec = nv_socioec;
+    }
+
+    public String getTipo_beca() {
+        return tipo_beca;
+    }
+
+    public void setTipo_beca(String tipo_beca) {
+        this.tipo_beca = tipo_beca;
+    }
     
 }
